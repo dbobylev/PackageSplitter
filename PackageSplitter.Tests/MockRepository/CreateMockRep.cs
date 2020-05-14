4d0f69625f7fc312cb0347033a87073e4dc98df3
Bobylev Dmitry <netlight4me@gmail.com>
2020-05-14 12:23:05 +0300

    save

---
+++ PackageSplitter.Tests/MockRepository/CreateMockRep.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PackageSplitter.Tests.MockRepository
+{
+    class CreateMockRep
+    {
+        [Test]
+        public static void CreateMockRepTest()
+        {
+            Directory.CreateDirectory("C:\\TestRep");
+            Directory.CreateDirectory("C:\\TestRep\\ALPHA");
+            Directory.CreateDirectory("C:\\TestRep\\BETA");
+            File.WriteAllText("C:\\TestRep\\ALPHA\\c_package.spc", TextCPackage.Spec);
+            File.WriteAllText("C:\\TestRep\\ALPHA\\c_package.bdy", TextCPackage.Body);
+
+            Assert.Pass();
+        }
+    }
+}
---
+++ PackageSplitter/src/Convertrs/OwnersToRepObjectsConverter.cs
@@ -0,0 +1,40 @@
+﻿using DataBaseRepository;
+using DataBaseRepository.Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace PackageSplitter.Convertrs
+{
+    class OwnersToRepObjectsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Seri.Log.Verbose($"OwnersToRepObjectsConverter begin");
+            
+            if (value == null || string.IsNullOrEmpty((string)value))
+                return new RepositoryObject[] { };
+
+            if (!(value is string))
+                throw new Exception("Wrong value in OwnersToRepObjectsConverter");
+
+
+            string Owner = (string)value;
+
+            SelectRequest request = new SelectRequest() { Owner = Owner, FileTypes = new List<eRepositoryObjectType>() { eRepositoryObjectType.Package_Body } };
+            RepositoryObject[] objects = DBRep.Instance().GetFiles(request).ToArray();
+
+            Seri.Log.Verbose($"OwnersToRepObjectsConverter find {objects.Count()} objects");
+
+            return objects;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
