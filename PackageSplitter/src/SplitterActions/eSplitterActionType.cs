e40bbcd297f110c7ef85e73a68adce6cfed80aaa
Bobylev Dmitry <netlight4me@gmail.com>
2020-05-15 18:26:45 +0300

    save

---
+++ PackageSplitter/src/SplitterActions/eSplitterActionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackageSplitter.SplitterActions
+{
+    enum eSplitterActionType
+    {
+        Add,
+        Delete,
+        MakeLink
+    }
+}
---
+++ PackageSplitter/src/SplitterActions/eSplitterObjectType.cs
@@ -0,0 +1,17 @@
+﻿using PackageSplitter.Templates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PackageSplitter.SplitterActions
+{
+    [Flags]
+    enum eSplitterObjectType
+    {
+        None = 0,
+        OldSpec = 1 << 0,
+        OldBody = 1 << 1,
+        NewSpec = 1 << 2,
+        NewBody = 1 << 3
+    }
+}
