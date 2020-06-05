2f73b257f87cf43e1eb2e08e7a4dfe45c658779f
Bobylev Dmitry <netlight4me@gmail.com>
2020-06-05 12:16:33 +0300

    save

--- PackageSplitter/src/View/Templates/SplitterCell.xaml.cs
+++ PackageSplitter/src/View/Templates/SplitterCell.xaml.cs
@@ -22,6 +22,8 @@ namespace PackageSplitter.View.Templates
     /// </summary>
     public partial class SplitterCell : UserControl
     {
+        private bool ButtonsWasLoaded = false;
+
         private static SplitterCellButtonFactory ButtonFactory = new SplitterCellButtonFactory();
 
         public static DependencyProperty ElementStateTypeProperty = DependencyProperty.Register("ElementStateType", typeof(eElementStateType), typeof(SplitterCell));
@@ -46,11 +48,19 @@ namespace PackageSplitter.View.Templates
 
         private void uc_Loaded(object sender, RoutedEventArgs e)
         {
-            var buttons = ButtonFactory.GetButtons(SplitterObjectType, (ePackageElementType)GetValue(PackageElementTypeProperty));
-            foreach (var item in buttons)
+            /* Создание кнопок вынесено из конструктора, так как там еще собраны не все данные для построения кнопок
+             * При прокрутке элементов, они пропадают с экрана и заново появляются, в этот момент повторно отрабатывает uc_Loaded
+             * Добавлен тоггл ButtonsWasLoaded, для единичной загрузки кнопок
+             */
+            if (!ButtonsWasLoaded)
             {
-                item.Click += Button_Click;
-                mainStack.Children.Add(item);
+                var buttons = ButtonFactory.GetButtons(SplitterObjectType, (ePackageElementType)GetValue(PackageElementTypeProperty));
+                foreach (var item in buttons)
+                {
+                    item.Click += Button_Click;
+                    mainStack.Children.Add(item);
+                }
+                ButtonsWasLoaded = true;
             }
         }
     }
